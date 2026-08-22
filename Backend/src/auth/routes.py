d22e510049e6b16e9807c94d8c546400dc76da06
dafiq khan <chingpoklee1@gmail.com>
2026-08-22 15:19:18 +0530

    Delete Backend/src/auth/routes.py

--- Backend/src/auth/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from src.auth.controller import signup, login, logout, get_current_user
-
-auth_bp = Blueprint("auth", __name__, url_prefix="/api/auth")
-
-auth_bp.route("/signup", methods=["POST"])(signup)
-auth_bp.route("/login", methods=["POST"])(login)
-auth_bp.route("/logout", methods=["POST"])(logout)
-auth_bp.route("/me", methods=["GET"])(get_current_user)
